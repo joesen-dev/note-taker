9f02b7d85f526a02a850587285043f9a76d4b789
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-08-06 12:45:30 -0400

    Updating repo

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require("express");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 const app = express(); //  instantiate the server
 
-app.get("/api/notes", (req, res) => {
-  res.send("Hello!"); // use for small files
-}); // add the route
+// Sets up the Express app to handle data parsing
+app.use(express.static("public")); // add middleware to specify the root ("public") directory to serve static files
+app.use(express.urlencoded({ extended: true })); // parse incoming string or array data
+app.use(express.json()); // parse incoming JSON data
+
+// get index.html file
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+}); // This is the route to the root of the server
+
+app.get("/index.js", (req, res) => {
+  res.json("index.js");
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+}); // This route goes to /notes
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+}); // any route that wasn't previously defined will fall under this request and will receive the homepage as the response
 
 app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+  console.log(`API server now on port http://localhost:${PORT}`);
 }); // tell server to listen for requests
